37e6e25a6c7a4e59821403eda965423e84d35db3
SachinBalagam <balagamsachin337@gmail.com>
2023-09-03 16:16:05 +0000

    Final Commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,1 +1,58 @@
 // Write your code here
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import TeamCard from '../TeamCard/index'
+import './index.css'
+
+class Home extends Component {
+  state = {teamsList: [], isLoading: true}
+
+  componentDidMount() {
+    this.getTeamList()
+  }
+
+  getTeamList = async () => {
+    const response = await fetch('https://apis.ccbp.in/ipl')
+    const data = await response.json()
+    const {teams} = data
+    // console.log(data)
+    const formattedData = teams.map(eachTeam => ({
+      id: eachTeam.id,
+      name: eachTeam.name,
+      teamImageUrl: eachTeam.team_image_url,
+    }))
+    // console.log(formattedData)
+    this.setState({teamsList: formattedData, isLoading: false})
+  }
+
+  render() {
+    const {teamsList, isLoading} = this.state
+    return (
+      <div className="bg-container">
+        {isLoading ? (
+          <div testid="loader">
+            <Loader type="Oval" color="#ffffff" height={50} width={50} />
+          </div>
+        ) : (
+          <div>
+            <div className="logo-title-container">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/ipl-logo-img.png"
+                alt="ipl logo"
+                className="ipl-logo"
+              />
+              <h1 className="title">IPL Dashboard</h1>
+            </div>
+            <ul className="teams-list-container">
+              {teamsList.map(eachTeam => (
+                <TeamCard key={eachTeam.id} data={eachTeam} />
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+export default Home
--- src/components/TeamCard/index.js
+++ src/components/TeamCard/index.js
@@ -1,1 +1,18 @@
 // Write your code here
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const TeamCard = props => {
+  const {data} = props
+  const {id, name, teamImageUrl} = data
+  return (
+    <Link to={`/team-matches/${id}`} className="link">
+      <li className="each-team-container">
+        <img src={teamImageUrl} alt={name} className="each-team-logo" />
+        <p className="each-team-name">{name}</p>
+      </li>
+    </Link>
+  )
+}
+
+export default TeamCard
